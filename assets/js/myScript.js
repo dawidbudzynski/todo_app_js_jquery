c6f8e517fab7badd3732f2d1446931dc72423868
dawid <budzynski.dawid@gmail.com>
2018-07-08 18:54:30 +0200

    styling

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -12,6 +12,10 @@ $("input[type='text']").keypress(function (event) {
     if (event.which === 13) {
         var itemValue = $(this).val();
         $(this).val('');
-        $('ul').append('<li><span>X</span> ' + itemValue + '</li>');
+        $('ul').append('<li><span><i class="fas fa-trash"></i></span> ' + itemValue + '</li>');
     }
+});
+
+$('.fa-plus').click(function () {
+    $("input[type='text']").fadeToggle();
 });
